0572d4436098aa3adc8d85c3242995f2d588ccfb
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2020-03-12 19:07:11 +0100

    update

---
+++ docs/precache-manifest.df8f1bdc5790f00d91c3ef7ceb6de306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb02e1f2dbbfd9bc01a82d102310439",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "f485898067d079217ea4",
+    "url": "/horizoverlay/static/css/main.e69aa429.chunk.css"
+  },
+  {
+    "revision": "3da39cffb0303236c141",
+    "url": "/horizoverlay/static/js/2.32e3f069.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/horizoverlay/static/js/2.32e3f069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f485898067d079217ea4",
+    "url": "/horizoverlay/static/js/main.b83ab655.chunk.js"
+  },
+  {
+    "revision": "e6fe32d5e674eb8f362b",
+    "url": "/horizoverlay/static/js/runtime-main.f0c0c1a1.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
